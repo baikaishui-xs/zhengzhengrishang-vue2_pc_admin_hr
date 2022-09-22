ed924843370f5cb06b128055b2ed02e44ae36d6e
郑先生 <1160206165@qq.com>
2022-09-22 17:53:05 +0800

    员工操作权限。因为后台的标识会重置，这样会导致一直没有权限，暂时注释掉

--- src/views/staff/index.vue
+++ src/views/staff/index.vue
@@ -13,7 +13,11 @@
       <div class="right-box">
         <el-button class="btn" type="success" @click="$router.push('importExcel')">excel 导入</el-button>
         <el-button class="btn1" type="danger" @click="exportData">excel 导出</el-button>
-        <el-button class="btn2" type="primary" :disabled="!checkPermission('add-staff')" @click="showDialog('新增员工')">新增员工</el-button>
+
+        <!-- 操作权限。因为后台的标识会重置，这样会导致一直没有权限，暂时注释掉 -->
+        <!-- <el-button class="btn2" type="primary" :disabled="!checkPermission('add-staff')" @click="showDialog('新增员工')">新增员工</el-button> -->
+        <el-button class="btn2" type="primary" @click="showDialog('新增员工')">新增员工</el-button>
+
       </div>
     </el-card>
 
@@ -42,13 +46,7 @@
     </el-table>
 
     <!-- 分页组件 -->
-    <el-pagination
-      :page-size="paginationObj.pagesize"
-      :current-page="paginationObj.page"
-      layout="prev, pager, next"
-      :total="paginationObj.total"
-      @current-change="changePage"
-    >
+    <el-pagination :page-size="paginationObj.pagesize" :current-page="paginationObj.page" layout="prev, pager, next" :total="paginationObj.total" @current-change="changePage">
     </el-pagination>
 
     <!-- 对话框 -->
@@ -94,7 +92,8 @@
     <el-dialog title="二维码" :visible="whetherShowQrCodeDialog" width="30%" @close="closureQrCodeDialog">
       <el-row type="flex" justify="center">
         <canvas ref="qrCodeCanvas">
-        </canvas></el-row>
+        </canvas>
+      </el-row>
     </el-dialog>
   </div>
 </template>
@@ -107,58 +106,62 @@ import QrCode from 'qrcode'
 export default {
   name: 'Staff',
   filters: {
-    formatDate // 格式化时间
+    formatDate, // 格式化时间
   },
   data() {
     return {
       tableList: [], // 表格数据
       demoList: [], // Demo 列表
-      paginationObj: { // 分页器对象
+      paginationObj: {
+        // 分页器对象
         page: 1, // 页码
         pagesize: 5, // 每页显示多少条数据
-        total: 0 // 共多少条数据
+        total: 0, // 共多少条数据
       },
-      dialogConfig: { // 对话框配置项
+      dialogConfig: {
+        // 对话框配置项
         isShowDialog: false, // 是否显示对话框
-        title: '' // 标题
+        title: '', // 标题
       },
       cornerList: [], // 员工列表
       checkList: [], // 选中项
       selectStaffId: 0, // 当前员工 ID
-      newEmployeeFormData: { // 新增员工 表单数据
+      newEmployeeFormData: {
+        // 新增员工 表单数据
         username: '',
         mobile: '',
         timeOfEntry: '',
         formOfEmployment: '',
         workNumber: '',
         departmentName: '',
-        correctionTime: ''
+        correctionTime: '',
       },
-      newEmployeeFormRules: { // 表单验证规则
+      newEmployeeFormRules: {
+        // 表单验证规则
         username: [
-          { required: true, trigger: 'blur', message: '不能为空' } // 不能为空
+          { required: true, trigger: 'blur', message: '不能为空' }, // 不能为空
         ],
         mobile: [
-          { required: true, trigger: 'blur', message: '不能为空' } // 不能为空
+          { required: true, trigger: 'blur', message: '不能为空' }, // 不能为空
         ],
         timeOfEntry: [
-          { required: true, trigger: 'blur', message: '不能为空' } // 不能为空
+          { required: true, trigger: 'blur', message: '不能为空' }, // 不能为空
         ],
         formOfEmployment: [
-          { required: true, trigger: 'blur', message: '不能为空' } // 不能为空
+          { required: true, trigger: 'blur', message: '不能为空' }, // 不能为空
         ],
         workNumber: [
-          { required: true, trigger: 'blur', message: '不能为空' } // 不能为空
+          { required: true, trigger: 'blur', message: '不能为空' }, // 不能为空
         ],
         departmentName: [
-          { required: true, trigger: 'blur', message: '不能为空' } // 不能为空
+          { required: true, trigger: 'blur', message: '不能为空' }, // 不能为空
         ],
         correctionTime: [
-          { required: true, trigger: 'blur', message: '不能为空' } // 不能为空
-        ]
+          { required: true, trigger: 'blur', message: '不能为空' }, // 不能为空
+        ],
       },
       defaultImg: require('@/assets/images/default.png'), // 考虑到打包时，路径会发送变化，导致图片加载失败。所以这里使用 require 包裹起来
-      whetherShowQrCodeDialog: false // 是否显示 二维码 对话框
+      whetherShowQrCodeDialog: false, // 是否显示 二维码 对话框
     }
   },
   created() {
@@ -166,7 +169,8 @@ export default {
     this.getAllTheCornerList()
   },
   methods: {
-    async getEmployeeList() { // 获取 员工列表
+    async getEmployeeList() {
+      // 获取 员工列表
       const result = await getEmployeeList({ page: this.paginationObj.page, size: this.paginationObj.pagesize })
       this.paginationObj.total = result.total
       this.tableList = result.rows
@@ -175,18 +179,22 @@ export default {
       const result = await getStaffInfo(id)
       this.checkList = result.roleIds
     },
-    async getAllTheCornerList() { // 获取 所有角色列表
+    async getAllTheCornerList() {
+      // 获取 所有角色列表
       const result = await getAllTheCornerList()
       this.cornerList = result.rows
     },
-    async delRole(id) { // 删除 角色
+    async delRole(id) {
+      // 删除 角色
       await delRole(id)
     },
-    changePage(newPage) { // 监听页码
+    changePage(newPage) {
+      // 监听页码
       this.paginationObj.page = newPage
       this.getEmployeeList()
     },
-    async sure() { // 确定
+    async sure() {
+      // 确定
       if (this.dialogConfig.title === '分配角色') {
         await updateRole({ id: this.selectStaffId, roleIds: this.checkList })
         this.$message.success('分配角色成功')
@@ -201,7 +209,8 @@ export default {
       }
       this.dialogConfig.isShowDialog = false
     },
-    cancel() { // 取消
+    cancel() {
+      // 取消
       if (this.dialogConfig.title === '新增员工') {
         setTimeout(() => {
           this.$refs.newEmployeeFormRef.resetFields()
@@ -209,7 +218,8 @@ export default {
       }
       this.dialogConfig.isShowDialog = false
     },
-    async showDialog(type, id) { // 显示对话框
+    async showDialog(type, id) {
+      // 显示对话框
       if (type === '分配角色') {
         this.dialogConfig.title = '分配角色'
         this.selectStaffId = id
@@ -220,41 +230,48 @@ export default {
       }
       this.dialogConfig.isShowDialog = true
     },
-    closureQrCodeDialog() { // 关闭 二维码 对话框
+    closureQrCodeDialog() {
+      // 关闭 二维码 对话框
       this.whetherShowQrCodeDialog = false
     },
-    showQrCodeDialog(url) { // 显示 二维码 对话框
+    showQrCodeDialog(url) {
+      // 显示 二维码 对话框
       this.whetherShowQrCodeDialog = true
 
-      this.$nextTick(() => { // 说明：显示对话框后数据已更新，但是结构还没渲染出来，所以需要使用 $nextTick 来等待结构渲染完毕
+      this.$nextTick(() => {
+        // 说明：显示对话框后数据已更新，但是结构还没渲染出来，所以需要使用 $nextTick 来等待结构渲染完毕
         // this.$nextTick 作用：等待结构渲染完毕后再去执行回调函数
         QrCode.toCanvas(this.$refs.qrCodeCanvas, url) // 转换成二维码
       })
     },
-    exportData() { // 将数据导出为 Excel 文件
-      import('@/vendor/Export2Excel').then(excel => { // 懒加载
+    exportData() {
+      // 将数据导出为 Excel 文件
+      import('@/vendor/Export2Excel').then(excel => {
+        // 懒加载
         const order = ['mobile', 'username', 'timeOfEntry', 'correctionTime', 'workNumber']
         /* 格式化数据：将数组中对象类型的成员格式化成数组类型的成员 && 筛选出指定的属性 && 成员中的数据按照规定的顺序排列 && 去掉成员中的数据 key 只保留 value
         格式化前：[{id: "1063705989926227968", mobile: "13800000002", username: "管理员"}, {id: "1063705989926227968", mobile: "13800000003", username: "孙财"}]
         格式化后：[['13800000002', '管理员'], ['13800000003', '孙财']]
         */
-        const newRows = this.tableList.map(item => Object.keys(order).map(key => {
-          // 日期转换
-          if (order[key] === 'timeOfEntry' || order[key] === 'correctionTime') {
-            return formatDate(item[order[key]])
-          }
+        const newRows = this.tableList.map(item =>
+          Object.keys(order).map(key => {
+            // 日期转换
+            if (order[key] === 'timeOfEntry' || order[key] === 'correctionTime') {
+              return formatDate(item[order[key]])
+            }
 
-          return item[order[key]]
-        }))
+            return item[order[key]]
+          })
+        )
 
         excel.export_json_to_excel({
           header: ['手机号', '姓名', '入职日期', '转正日期', '工号'],
           data: newRows,
-          filename: '员工工资表'
+          filename: '员工工资表',
         })
       })
-    }
-  }
+    },
+  },
 }
 </script>
 <style lang='scss' scoped>
@@ -269,11 +286,11 @@ export default {
       padding: 0 10px;
       display: flex;
       align-items: center;
-      border: 1px solid #91D5FF;
-      background: #E7F4FF;
+      border: 1px solid #91d5ff;
+      background: #e7f4ff;
       .el-icon-info {
         margin-right: 3px;
-        color: #409EFF;
+        color: #409eff;
       }
 
       .text {
@@ -284,19 +301,15 @@ export default {
       }
 
       .text2 {
-
       }
     }
 
     .right-box {
       .btn {
-
       }
       .btn1 {
-
       }
       .btn2 {
-
       }
     }
   }
